17eebbbef2d2a0fd4214e84c9b13ee8baf59df10
anish-maity <21f3000417@ds.study.iitm.ac.in>
2025-12-23 14:13:38 +0000

    Resolve merge conflict in index.html

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <script>
 
 import Navbar from './components/Navbar.vue';
@@ -28,35 +28,5 @@ export default{
 html {
   scroll-behavior: smooth;
 }
-=======
-<script>
 
-import Navbar from './components/Navbar.vue';
-import { RouterView } from 'vue-router';
-
-
-export default{
-  components: { Navbar},
-
-};
-
-</script>
-
-<template>
-  <div class="bg-gray-900 text-white min-h-screen">
-    <Navbar />
-    <main>
-      
-      <RouterView />
-    </main>
-  </div>
- 
-</template>
-
-
-<style>
-html {
-  scroll-behavior: smooth;
-}
->>>>>>> abe7342e9e6f1bd6e7a84178dfa0c792b222decb
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import './assets/main.css'
 
 
@@ -16,22 +16,4 @@ app.use(router)
 
 
 app.mount('#app')
-=======
-import './assets/main.css'
 
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './Router';
-
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-
-app.mount('#app')
->>>>>>> abe7342e9e6f1bd6e7a84178dfa0c792b222decb
